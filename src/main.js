6a974189d86a1347fb413c73d9bdfd018130e8ee
Patrick Avery <patrick.avery@kitware.com>
2020-05-07 16:45:05 -0400

    Install girder and update packages
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 
+import GirderProvider from './plugins/girder';
+
 Vue.config.productionTip = false;
 
 new Vue({
+  provide: GirderProvider,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
