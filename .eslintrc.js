cd3c4a5e84b459ca6934d22caded56c37f5a2f81
Patrick Avery <patrick.avery@kitware.com>
2020-05-07 16:13:17 -0400

    Use prettier and airbnb formatting
    This also reformats the code.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/prettier'],
+  rules: {
+    'import/extensions': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
