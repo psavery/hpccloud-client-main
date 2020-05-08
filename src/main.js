7a23c752beca55ec9aa8b429ea577a7030e6b3e9
Patrick Avery <patrick.avery@kitware.com>
2020-05-08 11:11:52 -0400

    Add vuex
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 
 import GirderProvider from './plugins/girder';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   provide: GirderProvider,
   vuetify,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
