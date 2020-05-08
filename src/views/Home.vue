fce200490e053aa3995fd2e872c43d0d9e181153
Patrick Avery <patrick.avery@kitware.com>
2020-05-08 16:18:29 -0400

    Improve the landing page. Use MD icon.
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
+<style>
+.center {
+  text-align: center;
+  margin: auto;
+}
+</style>
+
 <template>
-  <div class="home">
-    Better landing page... You need to be logged in to do something...
+  <div class="center">
+    <br />
+    <br />
+    <v-icon size="250">mdi-cloud-download</v-icon>
+    <h2>Welcome to HPCCloud</h2>
+    <p>
+      Get started by <router-link to="/Register">registering</router-link> or
+      <router-link to="/Login">logging in</router-link>.
+    </p>
   </div>
 </template>
 
